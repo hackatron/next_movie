cadea61e04009b45dd9ca16eaa74f57cc36ce20a
Matteo Depalo <matteodepalo@gmail.com>
2013-04-08 17:18:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NextHcktrnMovie::Application.config.secret_key_base = 'e5ccb3760e0d6cb50ea1e6eed09322e4830d834a3b77f0d5d2dec4e0f78305c36c7bc02eed452e0c4ee5215e306480b1323bad9a4a1de7eac28e6c0575c27fcf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NextHcktrnMovie::Application.config.session_store :encrypted_cookie_store, key: '_next_hcktrn_movie_session'

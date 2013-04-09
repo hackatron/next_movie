2190ee3d7fcb82291a505432196c0b618ff75094
Luca Milan <lmilan@goresponsa.com>
2013-04-09 15:48:02 +0200

    Added json responses for single movie and all movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,15 @@
 class MoviesController < ApplicationController
   def show
-    
+    @movie = Movie.find(params[:id])
+    respond_to do |format|
+      format.json { render json: @movie }
+    end
+  end
+
+  def index
+    @movies = Movie.all
+    respond_to do |format|
+      format.json { render json: @movies }
+    end
   end
 end

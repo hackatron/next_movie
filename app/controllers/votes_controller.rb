cd55ae33722b470b797107d258ca5cc66ea40b2e
Matteo Depalo <matteodepalo@gmail.com>
2013-04-13 11:32:21 +0200

    Movie is now reloaded in vote create action
    Reason: the serializer was not using the movie with the new rating

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,9 @@ class VotesController < ApplicationController
 
   def create
     movie = Movie.find(params[:vote][:movie_id])
+    vote = movie.votes.create
+    movie.reload
 
-    respond_with movie.votes.create
+    respond_with vote
   end
 end
